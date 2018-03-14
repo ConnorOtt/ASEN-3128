26d418f89e2a351db14a5397cc9786ea3ee8bb98
Connor Ott <connor.ott@colorado.edu>
2018-03-14 15:52:01 -0600

    Modifying to include control gains

--- Assignment 7/assignment7Main.m
+++ Assignment 7/assignment7Main.m
@@ -9,7 +9,7 @@ mpf = 0.3048;    % meters per foot
 h = 40000 * mpf; % [ft]
 [~, ~, ~, rho] = atmosisa(h);
 
-%% Problem 1 - Aerodynamic Derivatives
+%% Assignment 6 Problem 1 - Aerodynamic Derivatives
 % Values from table 6.1
 C_X_u = -0.108;
 C_xa = 0.2193;
@@ -22,7 +22,7 @@ C_zq = -5.921;
 C_zahat = 5.896;
 
 C_mu = 0.1043;
-C_ma = -1.023;
+C_ma = -1.023; % Part a) modification
 C_mq = -23.92;
 C_mahat = -6.314;
 
@@ -72,11 +72,11 @@ input.booktabs = 1;
 input.tableCaption = 'Aerodynamic Derivatives';
 input.tableLabel = 'aeroDer';
 % call latexTable:
-latex = latexTable(input); % Maybe don't output this every time
+% latex = latexTable(input); % Maybe don't output this every time
 
 
 
-%% Problem 2 - A Matrix
+%% Assignment 6 Problem 2 - A Matrix
 W = 2.83176e6; % [N]
 g = 9.81; % [m/s^2] 
 m = W/g; % [kg]
@@ -106,10 +106,10 @@ input.booktabs = 1;
 input.tableCaption = '';
 input.tableLabel = '';
 % call latexTable:
-latex = latexTable(input); % Maybe don't output this every time
+% latex = latexTable(input); % Maybe don't output this every time
 
 
-%% Problem 3 - eigs, zeta, omega_n
+%% Assignment 6 Problem 3 - eigs, zeta, omega_n
 phuVal = vals(3:4);
 shortVal = vals(1:2);
 
@@ -120,45 +120,118 @@ w_nShort = abs(shortVal(1));
 zetaPhu = -real(phuVal(1))/w_nPhu;
 zetaShort = -real(shortVal(1))/w_nShort;
 
-
-%% Problem 4 - Approximations vs. Full A Matrix
-lamSaprx(:, 1) = [ (M_q/(2*Iy) + 1/(2*Iy)*sqrt(M_q^2 + 4*Iy*u_0*M_w));
+%% Assignment 6 Problem 4 - Approximations vs. Full A Matrix
+lamSAprx(:, 1) = [ (M_q/(2*Iy) + 1/(2*Iy)*sqrt(M_q^2 + 4*Iy*u_0*M_w));
                    (M_q/(2*Iy) - 1/(2*Iy)*sqrt(M_q^2 + 4*Iy*u_0*M_w)) ];
-                                
+
+w_nSAprx = abs(lamSAprx(1));
+zetaSAprx = -real(real(lamSAprx(1)))/w_nSAprx;
 TPhu_aprx =  pi * sqrt(2) * u_0 / g;
 TPhu = 2*pi/imag(phuVal(1));
 
-%% Problem 5 - ODE Sim
-initCondsMat = diag([10, 10, 0.1, 0.1]);
-initCondsMat = [0, 0, 0, 0; initCondsMat];
-tSpanLong = [0, 300];
-tSpanShort = [0, 10];
-titlesS = {'Trim State', ...
-          'Initial $\Delta u$ of 10 m/s - Short Period', ...
-          'Initial $\Delta w$ of 10 m/s - Short Period', ...
-          'Initial $\Delta q$ of 0.1 rad/s - Short Period', ...
-          'Initial $\Delta \theta$ of 0.1 rad - Short Period'};
-titlesP = {'Trim State', ...
-          'Initial $\Delta u$ of 10 m/s - Phugoid', ...
-          'Initial $\Delta w$ of 10 m/s - Phugoid', ...
-          'Initial $\Delta q$ of 0.1 rad/s - Phugoid', ...
-          'Initial $\Delta \theta$ of 0.1 rad - Phugoid'};
-yLabels = {'$\Delta u$ [m/s]', '$\Delta w$ [m/s]', ...
-           '$\Delta q$ [rad/s]', '$\Delta \theta$ [rad]'};
-printTitlesS = {'Trim', 'deltaU', 'deltaW','deltaQ', 'deltaTheta'};
-printTitlesP = {'Trim', 'deltaUPhu', 'deltaWPhu', ...
-                'deltaQPhu', 'deltaThetaPhu'};
-for i = 1:length(initCondsMat)
-   [t_s, F_s] = ode45(@(t, F)longSimODE(t, F, A), tSpanShort, ...
-                                    initCondsMat(i, :)); 
-   [t_p, F_p] = ode45(@(t, F)longSimODE(t, F, A), tSpanLong, ...
-                                    initCondsMat(i, :));
-   % Short Period Plots
-   plotPlots(t_s, F_s, titlesS{i}, yLabels, printTitlesS{i})
+
+%% Assignment 7 Problem 2 - Adding Control Derivatives
+C_xde = -3.818e-6;
+C_zde = -0.3648;
+C_mde = -1.444;
+
+X_de = C_xde * 0.5 * rho * u_0^2 * S;
+Z_de = C_zde * 0.5 * rho * u_0^2 * S;
+M_de = C_mde * 0.5 * rho * u_0^2 * S * cbar;
+
+% disp({X_de, Z_de, M_de})
+
+% X_dp = C_xdp * 0.5 * rho * u_0^2 * S;
+% Y_dp = C_zdp * 0.5 * rho * u_0^2 * S;
+% M_dp = C_mdp * 0.5 * rho * u_0^2 * S * cbar;
+
+%% Assignment 7 - Problem 3
+% Creating the B matrix
+B = zeros(4, 2);
+B(1:3, 1) = [X_de/m, Z_de/(m-Z_wdot), M_de/Iy + M_wdot/(Iy*(m-Z_wdot))]';
+
+% Choosing gains based on short period approximation of longitudinal
+% dynamics. 
+
+k_s = 1:0.01:20;
+[k1, k2, imVecPlusApx, imVecMinApx, reVecApx] = deal(zeros(length(k_s), 1));
+
+for i = 1:length(k_s)
+   k1(i) = (1-sqrt(k_s(i)))*M_q/M_de;
+   k2(i) = (1-k_s(i))*u_0*M_w/M_de;
    
-   % Long Period Plots
-   plotPlots(t_p, F_p, titlesP{i}, yLabels, printTitlesP{i})
+   K_short = [k1(i), k2(i); 0, 0];
+   Aaprx = [M_q/Iy, u_0*M_w/Iy; 1, 0];
+   A_new = Aaprx - M_de/Iy * K_short;
+   [~, svals] = eig(A_new, 'vector');
+   
+   imVecPlusApx(i) = imag(svals(1));
+   imVecMinApx(i) = imag(svals(2));
+   reVecApx(i) = real(svals(1));
+  
 end
+
+%% Using short period approximation gains in full linearized dynamics
+
+[imVecPlus, imVecMin, reVec] = deal(zeros(length(k_s), 1));
+for i = 1:length(k_s)
+    K_cell = zeros(2, 4);
+    K_cell(1, 3:4) = [k1(i), k2(i)];
+    
+    A_star = A - B*K_cell; % Closed loop matrix
+    [~, vals] = eig(A_star, 'vector');
+    imVecPlus(i) = imag(vals(1));
+    imVecMin(i) = imag(vals(2));
+    reVec(i) = real(vals(1));
+end
+
+figure
+hold on; grid on; grid minor;
+
+plot(reVec, imVecPlus, 'k', 'linewidth', 1.1);
+plot(reVec, imVecMin, 'm', 'linewidth', 1.1);
+plot(reVecApx, imVecPlusApx, 'b', 'linewidth', 1.1);
+plot(reVecApx, imVecMinApx, 'r', 'linewidth', 1.1);
+
+leg = legend('Full Dynamics Positive Pair', ...
+             'Full Dynamics Negative Pair', ...
+             'Approximation Positive Pair', ...
+             'Approximation Negative Pair');
+title('Short Mode Full Dynamics and Approximation Eigenvalues');
+xlabel('Real Values');
+ylabel('Imaginary Values');
+
+% %% Assignment 6 - Problem 5
+% initCondsMat = diag([10, 10, 0.1, 0.1]);
+% initCondsMat = [0, 0, 0, 0; initCondsMat];
+% tSpanLong = [0, 300];
+% tSpanShort = [0, 10];
+% titlesS = {'Trim State', ...
+%           'Initial $\Delta u$ of 10 m/s - Short Period', ...
+%           'Initial $\Delta w$ of 10 m/s - Short Period', ...
+%           'Initial $\Delta q$ of 0.1 rad/s - Short Period', ...
+%           'Initial $\Delta \theta$ of 0.1 rad - Short Period'};
+% titlesP = {'Trim State', ...
+%           'Initial $\Delta u$ of 10 m/s - Phugoid', ...
+%           'Initial $\Delta w$ of 10 m/s - Phugoid', ...
+%           'Initial $\Delta q$ of 0.1 rad/s - Phugoid', ...
+%           'Initial $\Delta \theta$ of 0.1 rad - Phugoid'};
+% yLabels = {'$\Delta u$ [m/s]', '$\Delta w$ [m/s]', ...
+%            '$\Delta q$ [rad/s]', '$\Delta \theta$ [rad]'};
+% printTitlesS = {'Trim', 'deltaU', 'deltaW','deltaQ', 'deltaTheta'};
+% printTitlesP = {'Trim', 'deltaUPhu', 'deltaWPhu', ...
+%                 'deltaQPhu', 'deltaThetaPhu'};
+% for i = 1:length(initCondsMat)
+%    [t_s, F_s] = ode45(@(t, F)longSimODE(t, F, A), tSpanShort, ...
+%                                     initCondsMat(i, :)); 
+%    [t_p, F_p] = ode45(@(t, F)longSimODE(t, F, A), tSpanLong, ...
+%                                     initCondsMat(i, :));
+% %    % Short Period Plots
+% %    plotPlots(t_s, F_s, titlesS{i}, yLabels, printTitlesS{i})
+% %    
+% %    % Long Period Plots
+% %    plotPlots(t_p, F_p, titlesP{i}, yLabels, printTitlesP{i})
+% end
 
 
 
--- Assignment 7/longSimODE.m
+++ Assignment 7/longSimODE.m
@@ -1,6 +1,3 @@
-function dfdt = longSimODE(t, F, A)
-
-% Lol
-dfdt = A*F;
-
+function dfdt = longSimODE(t, F, A, B, K)
+dfdt = A*F(1:4) - B*K*F(1:4);
 end
